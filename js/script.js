5f30fe6fe8690af076d2aa6f883a4f4a9d9e69c2
Stephanie Alcantara <x4ll4nx@gmail.com>
2011-10-14 12:39:28 +0100

    images added, css and js updates, some small HTML changes

--- js/script.js
+++ js/script.js
@@ -18,6 +18,46 @@ $(document).ready(function(){
 	if ($.fn.placeholder) {
 		$('input, textarea').placeholder();		
 	}
+	
+	var vpheight = $(window).height();
+	var vpwidth = $(window).width();
+	$('#event-form-wrapper').css('min-height', vpheight - 40)
+console.log(vpwidth);
+
+	if(vpwidth <= 767){
+	 
+	
+		$('#my-events-wrapper li').click(function(){
+			
+			$('#my-events-wrapper').animate({
+			    left: '-' + vpwidth
+			  }, 1000, function() {
+			    $(this).hide();
+			  });
+			  
+			$('#event-form-wrapper').show().animate({
+			    left: '0'
+			  }, 1000, function() {
+			  	$('nav li:first-child').show();
+			  });
+		});
+		
+		$('nav a:first-child').click(function(){
+			$('#event-form-wrapper').animate({
+			    left: '100%'
+			  }, 1000, function() {
+			    $(this).hide();
+			  });
+			  
+			$('#my-events-wrapper').show().animate({
+			    left: '0'
+			  }, 1000, function() {
+			    $('nav li:first-child').hide();
+			  });
+			return false;
+		});
+		
+	}
 });
 
 
